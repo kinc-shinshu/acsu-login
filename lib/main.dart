de2ffd190af5167cca090495623ea6bada7b2360
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2019-05-12 17:57:27 +0900

    :sparkles: add base post func

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -17,11 +18,14 @@ class MyApp extends StatelessWidget {
 
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _LoginPageState createState() => new _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   bool _checkboxSelected = true;
+  TextEditingController _uidController = new TextEditingController();
+  TextEditingController _pwdController = new TextEditingController();
+  GlobalKey _formKey= new GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -30,47 +34,96 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: Text("大学Wifiに接続しよう"),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              autofocus: true,
-              decoration: InputDecoration(
-                labelText: "メールアドレス",
-                hintText: "大学のメールアドレス",
-                prefixIcon: Icon(Icons.person)
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                autofocus: true,
+                controller: _uidController,
+                decoration: InputDecoration(
+                    labelText: "メールアドレス",
+                    hintText: "大学のメールアドレス",
+                    prefixIcon: Icon(Icons.person)
+                ),
+                validator: (v) {
+                  return v
+                      .trim()
+                      .length > 0 ? null : "メールアドレス入れてくれよ";
+                }
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "パスワード",
-                hintText: "流石に覚えてるよね？",
-                prefixIcon: Icon(Icons.lock)
+              TextFormField(
+                controller: _pwdController,
+                decoration: InputDecoration(
+                    labelText: "パスワード",
+                    hintText: "流石に覚えてるよね？",
+                    prefixIcon: Icon(Icons.lock)
+                ),
+                obscureText: true,
+                validator: (v) {
+                  return v
+                      .trim()
+                      .length > 0 ? null : "パスワードを入れてくれよな";
+                },
               ),
-              obscureText: true,
-            ),
-            CheckboxListTile(
-              title: Text("ログイン情報を保存しますか？"),
-              value: _checkboxSelected,
-              activeColor: Colors.blue,
-              onChanged: (e){
-                setState(() {
-                  _checkboxSelected = e;
-                });
-              },
-              // ここでcheckboxを左側に置いてる
-              controlAffinity: ListTileControlAffinity.leading,
-            ),
-            RaisedButton(
-              child: Text("ログイン"),
-              onPressed: () => {
-                // ここにログイン処理
-                // POSTかな？
-              },
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: CheckboxListTile(
+                        title: Text("ログイン情報を保存しますか？"),
+                        value: _checkboxSelected,
+                        activeColor: Colors.blue,
+                        onChanged: (e){
+                          setState(() {
+                            _checkboxSelected = e;
+                          });
+                        },
+                        // ここでcheckboxを左側に置いてる
+                        controlAffinity: ListTileControlAffinity.leading,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 14.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: RaisedButton(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text("ログインする"),
+                        color: Theme
+                            .of(context)
+                            .primaryColor,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          if((_formKey.currentState as FormState).validate()){
+                            postLoginData(_uidController.text, _pwdController.text);
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                )
+              )
+            ],
+          )
         ),
       ),
     );
+  }
+}
+
+void postLoginData(uid, pwd) async {
+  try {
+    Response response = await Dio().post("https://login.shinshu-u.ac.jp/cgi-bin/Login.cgi", data: {"uid": uid, "pwd": pwd});
+    print(response);
+  } catch (e) {
+    print(e);
   }
 }
